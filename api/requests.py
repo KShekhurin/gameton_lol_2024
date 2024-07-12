66189f7259c8da714e932c1f3574b62abe667724
Shekhurin Kirill <k.shekhurin@gmail.com>
2024-07-12 19:17:33 +0000

    api + models fro sep request

---
+++ api/requests.py
@@ -0,0 +1,13 @@
+from ../setup import API_TOKEN
+import requests as req
+
+headers = { 'X-Auth-Token': API_TOKEN }
+
+def make_step():
+    pass
+
+def make_step():
+    pass
+
+def make_step():
+    pass
---
+++ models/data_models.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+class Attack:
+    def __init__(self, build_id: str, target: Point):
+        self.build_id = build_id
+        self.target = target
+
+class StepData:
+    def __init__(self, attacks: List[Attack], builds: List[Point], base_pos: Point):
+        self.attacks = attacks
+        self.builds = builds
+        self.base_pos = base_pos
