edac2ad66f7d60ddac51809bfa4c87e9240d7ed8
Shekhurin Kirill <k.shekhurin@gmail.com>
2024-07-12 19:51:39 +0000

    premerge commit

--- api/requests.py
+++ api/requests.py
@@ -1,10 +1,13 @@
 from ../setup import API_TOKEN
+from ../models.data_models import *
+
 import requests as req
+
 
 headers = { 'X-Auth-Token': API_TOKEN }
 
-def make_step():
-    pass
+def make_step(data: StepData):
+    
 
 def make_step():
     pass
--- setup.py
+++ setup.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
+SERVER_ADRESS = os.getenv('SERVER_ADRESS')
