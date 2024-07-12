214cdee8b66acfff9cbaee9486aa4d56848d5523
Usimon71 <simonudaltsov@gmail.com>
2024-07-12 20:37:59 +0300

    my 4th contribution

--- models/data_models.py
+++ models/data_models.py
@@ -1,20 +1,19 @@
 from typing import List
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def as_dict(self):
+        d = {"x" : self.x,
+            "y" : self.y}
+
+        return d
+
 class StepData:
-
-    class Point:
-        def __init__(self, x: int, y: int):
-            self.x = x
-            self.y = y
-
-        def as_dict(self):
-            d = {"x" : self.x,
-                "y" : self.y}
-
-            return d
-        
     class Attack:
-        def __init__(self, block_id: str, target: Point):
+        def __init__(self, block_id: str, target : Point):
             self.block_id = block_id
             self.target = target
 
@@ -28,7 +27,7 @@ class StepData:
         self.attacks = attacks
         self.builds = builds
         self.base_pos = base_pos
-        
+
     def as_dict(self):
         d = {"attacks" : [x.as_dict() for x in self.attacks],
              "builds" : [x.as_dict() for x in self.builds],
--- setup.py
+++ setup.py
@@ -4,4 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
-SERVER_ADRESS = os.getenv('SERVER_ADRESS')
