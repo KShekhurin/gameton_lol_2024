4d13c3f2969e90ee7cdd3ea55d00e2853252de92
pess-sander <ivankosmynin5560@gmail.com>
2024-07-13 10:42:56 +0300

    trabla...

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from setup import API_TOKEN
 from models.data_models import *
+import matplotlib.pyplot as plt
 
-from api.requests import participate, look_around
+from api.requests import participate, look_around, get_rounds
 
-try:
-    participate()
-    look_around()
-except Exception as exc:
-    print(exc)
+while (True):
+    try:
+        rounds = get_rounds()
+        print(rounds)
+        print(len(rounds.rounds))
+        for r in rounds.rounds:
+            print(r.name)
+
+        break
+    except Exception as exc:
+        print(exc)
