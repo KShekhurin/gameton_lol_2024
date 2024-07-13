670704edd22594bdaa9e79ce727f45c2d605e7f5
pess-sander <ivankosmynin5560@gmail.com>
2024-07-13 11:31:31 +0300

    sleepy

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from setup import API_TOKEN
 from models.data_models import *
-import matplotlib.pyplot as plt
 
-from api.requests import participate, look_around, get_rounds
+from api.requests import participate, look_around, get_rounds, watch_eternity
+from bussiness import wait_till_nearest
+
+game = None
+nearest_round = None
 
 while (True):
     try:
-        rounds = get_rounds()
-        print(rounds)
-        print(len(rounds.rounds))
-        for r in rounds.rounds:
-            print(r.name)
+        game = get_rounds()
+        print(game.game_name)
+
+        game.rounds.sort(key=lambda x: x.start_at)
+        for r in game.rounds:
+            if r.status == 'not started':
+                nearest_round = r
+                break
 
         break
+    except Exception as exc:
+        print(exc)
+
+wait_till_nearest(nearest_round)
+
+while (True):
+    try:
+        participate()
     except Exception as exc:
         print(exc)
